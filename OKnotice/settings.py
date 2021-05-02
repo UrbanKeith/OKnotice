2671e71c9e9bb356db2311fa7813032c53fe75dc
UrbanKeith <71178373+UrbanKeith@users.noreply.github.com>
2021-05-02 15:57:15 +0400

    configurate db

--- OKnotice/settings.py
+++ OKnotice/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'OKnotice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
